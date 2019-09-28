5b4859619bcbb241a78941f53f3e61896b79edf6
lerdof <55908845+lerdof@users.noreply.github.com>
2019-09-28 09:39:13 +0300

    Create Bot.js

---
+++ Bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NjI3MzkzNjY3MDg2Mjg2ODY4.XY7__w.pyHheTBezVYur1Qbshwyo6_Wwos');
